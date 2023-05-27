e647b67bcbfc84f635755e0b86610778d5fde8a9
yusuf <majorhaji@github.com>
2023-05-27 01:08:45 +0100

    menu function added

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
