9b55d85982079af7cae4c33fe5167404b3a8b9b5
yusuf <majorhaji@github.com>
2023-06-20 15:44:12 +0100

    styling and email contact added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
-import { CoursesComponent } from './courses/courses.component';
+
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
 import { IntroComponent } from './intro/intro.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HeaderComponent, AboutComponent, TestimonialsComponent, CoursesComponent, FooterComponent, ContactComponent, IntroComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    AboutComponent,
+    TestimonialsComponent,
+
+    FooterComponent,
+    ContactComponent,
+    IntroComponent,
+    CoursesComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
 
+@Injectable()
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  message?: string;
 
+  constructor(private http: HttpClient) {}
+
+  submitForm(): void {
+    const formData = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      message: this.message,
+    };
+    this.http
+      .post(
+        'https://backend-for-family-youth-wellbeing.onrender.com/contact',
+        formData,
+        { responseType: 'text' }
+      )
+      .pipe(
+        tap(() => {
+          console.log('Message sent successfully!');
+          // Add any success message or further actions here
+        }),
+        catchError((error) => {
+          console.error('An error occurred:', error);
+          // Handle the error and display an error message
+          throw error; // Rethrow the error to propagate it to the error handler
+        })
+      )
+      .subscribe();
+  }
 }
